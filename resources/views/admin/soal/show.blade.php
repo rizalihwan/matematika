acda0a000d1e9d3dd050f68aae2c9c74fbd3f1b8
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-02-23 23:08:32 +0700

    fix show

--- resources/views/admin/soal/show.blade.php
+++ resources/views/admin/soal/show.blade.php
@@ -4,47 +4,30 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    <h3>Latihan Soal</h3>
+                    <h3><u>Detail Soal</u></h3>
                 </div>
                 <div class="card-body">
-                    <table width="100%">
-                        <tr>
-                            <th>
-                                1.
-                            </th>
-                            <th>{{ $soal->soal }}</th>
-                        </tr>
-                        <tr>
-                            <th>
-                            </th>
-                            <th>A. {{ $soal->pilihan_a }}</th>
-                        </tr>
-                        <tr>
-                            <th>
-                            </th>
-                            <th>B. {{ $soal->pilihan_b }}</th>
-                        </tr>
-                        <tr>
-                            <th>
-                            </th>
-                            <th>C. {{ $soal->pilihan_c }}</th>
-                        </tr>
-                        <tr>
-                            <th>
-                            </th>
-                            <th>D. {{ $soal->pilihan_d }}</th>
-                        </tr>
-                        <tr>
-                            <th>
-                            </th>
-                            <th>E. {{ $soal->pilihan_e }}</th>
-                        </tr>
-                        <tr>
-                            <th>
-                            </th>
-                            <th>Jawaban benar : {{ $soal->jawaban_benar }}</th>
-                        </tr>
-                    </table>
+                    <div>
+                        <b style="font-size: 1.3rem;">Soal : </b> <label style="font-size: 1.2rem;">{!! $soal->soal !!}</label>
+                    </div>
+                    <div>
+                        <b style="font-size: 1rem;">A.</b> <label style="font-size: 1rem;">{!! $soal->pilihan_a !!}</label>
+                    </div>
+                    <div>
+                        <b style="font-size: 1rem;">B.</b> <label style="font-size: 1rem;">{!! $soal->pilihan_b !!}</label>
+                    </div>
+                    <div>
+                        <b style="font-size: 1rem;">C.</b> <label style="font-size: 1rem;">{!! $soal->pilihan_c !!}</label>
+                    </div>
+                    <div>
+                        <b style="font-size: 1rem;">D.</b> <label style="font-size: 1rem;">{!! $soal->pilihan_d !!}</label>
+                    </div>
+                    <div>
+                        <b style="font-size: 1rem;">E.</b> <label style="font-size: 1rem;">{!! $soal->pilihan_e !!}</label>
+                    </div>
+                    <div>
+                        <b style="font-size: 1rem; background-color: rgb(102, 255, 0);">Jawaban Benar : </b> <label style="font-size: 1rem;">{!! $soal->jawaban_benar !!}</label>
+                    </div>
                 </div>
                 <div class="card-footer">
                     <a href="{{ route('admin.soal.index') }}" class="btn btn-danger mr-2"
